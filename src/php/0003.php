4263f8bc8ae202d299826d5a5da36458a1199b63
Burnz <benjamindiazf@openmailbox.org>
2015-05-07 18:35:13 -0300

    Added comments to code

--- src/php/0003.php
+++ src/php/0003.php
@@ -3,12 +3,15 @@
 	function largestPrimeFactor($n) {
 		$primeFactors = array();
 
+
 		function largestFactor($n) {
 			$smallest = smallestFactor($n);
 			$primeFactors[] = $smallest;
+			//if ($n == $smallest) returns largest prime factor, else calls the function recursively with ($n / $smallest) as param
 			return ($n == $smallest) ? max($primeFactors) : largestFactor($n / $smallest);
 		}
 
+		//Gets smallest prime factor of $n
 		function smallestFactor($n) {
 			for ($i = 2; $i <= $n ; $i++) { 
 				if ($n % $i == 0) {
